0b1c8dada054484faa3544cf386ca1eee44d164e
Bnyro <bnyro@tutanota.com>
2022-12-16 17:28:37 +0100

    Fix the wallpaper path

--- sway/scripts/bg.sh
+++ sway/scripts/bg.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 pkill swaybg
-swaybg -o eDP-1 -i $(find ~/Documents/Media/Wallpaper/ | shuf -n 1) -m fill &
+swaybg -o eDP-1 -i $(find ~/Projects/Artwork/Wallpaper/ | shuf -n 1) -m fill &
