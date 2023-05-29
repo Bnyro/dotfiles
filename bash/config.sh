5ae7832e6a651dbb7093b82b8ad5d5660a50bcb0
Bnyro <bnyro@tutanota.com>
2023-05-29 21:49:32 +0200

    Switch to using bash with ble.sh

---
+++ bash/config.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+# Aliases
+alias ls="exa --long --all --tree --level=1 --icons"
+alias df="duf /"
+alias wt="curl wttr.in/\?0"
+alias f="fzf --preview 'clear && bat --color=always {}'"
+
+# Path ENV
+export PATH="$PATH:/usr/local/bin"
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+eval "$(starship init bash)"
+eval "$(zoxide init bash)"
+
+# Write the following line near the top of .bashrc
+[[ $- == *i* ]] && source "$HOME/blesh/ble.sh" --rcfile "$HOME/.blerc"
+
+# Normal settings can be placed in between.
+
+# Write the following line at the bottom of .bashrc
+[[ ${BLE_VERSION-} ]] && ble-attach
+
+# Show system information
+clear && bfetch
